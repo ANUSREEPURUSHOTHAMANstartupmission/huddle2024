01f6768a0a8bf0e1d63fc9f9180fb44c2e4d3698
ANUSREEPURUSHOTHAMANstartupmission <anusree@startupmission.in>
2024-08-07 15:27:50 +0530

    changes

--- src/components/Hotels.svelte
+++ src/components/Hotels.svelte
@@ -41,7 +41,7 @@
 {#if popup_open}
 
   <div class="" >
-        <div  class="fixed inset-0 flex  items-center justify-center z-50 bg-black bg-opacity-50 ">
+        <div  class="fixed inset-0 flex  items-center justify-center z-50 bg-black bg-opacity-50 m-6 ">
     
             <div class=" bg-white  p-6">
 
