22772b322ea45af3204e13d187bdedb53c2c7afc
ANUSREEPURUSHOTHAMANstartupmission <anusree@startupmission.in>
2024-08-07 14:56:30 +0530

    changes

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			'sans': ['Lato', 'ui-sans-serif', 'system-ui', '-apple-system'],
+			'heading' : ['selimaregular', 'ui-sans-serif', 'system-ui', '-apple-system'],
+			'spl' : ['tajur_puncakregular', 'ui-sans-serif', 'system-ui', '-apple-system']
+		},
+		extend: {
+			colors: {
+				'huddle': '#C70000',
+			}
+		},
+	},
+	plugins: [
+		require("tailwindcss-scoped-groups"),
+		require('flowbite/plugin'),
+	],
+}
